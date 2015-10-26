1bc17eb2ad82178fd8b362a4d1877e4eb326a994
Dan Buckland <danbucklan@gmail.com>
2015-10-26 22:34:52 +0000

    Add gitapi interface and gitmodel class

---
+++ app/src/main/java/com/blocksolid/retrofittutorial/API/gitapi.java
@@ -0,0 +1,7 @@
+package com.blocksolid.retrofittutorial.API;
+
+/**
+ * Created by Dan Buckland on 26/10/2015.
+ */
+public interface gitapi {
+}
---
+++ app/src/main/java/com/blocksolid/retrofittutorial/model/gitmodel.java
@@ -0,0 +1,7 @@
+package com.blocksolid.retrofittutorial.model;
+
+/**
+ * Created by Dan Buckland on 26/10/2015.
+ */
+public class gitmodel {
+}
