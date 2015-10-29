e12c088ad179b2b134dafbb909d7544cb4af5cb1
Dan Buckland <danbucklan@gmail.com>
2015-10-29 14:01:46 +0000

    Add UI and logic for user search

--- app/src/main/java/com/blocksolid/retrofittutorial/API/gitapi.java
+++ app/src/main/java/com/blocksolid/retrofittutorial/API/gitapi.java
@@ -1,7 +1,19 @@
 package com.blocksolid.retrofittutorial.api;
 
+import com.blocksolid.retrofittutorial.model.GitModel;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
 /**
  * Created by Dan Buckland on 26/10/2015.
  */
 public interface GitApi {
+    @GET("/users/{user}")
+    //here is the other url part.best way is to start using /
+
+    void getFeed(@Path("user") String user, Callback<GitModel> response);
+    //string user is for passing values from edittext for eg: user=basil2style,google
+    //response is the response from the server which is now in the POJO
 }
--- app/src/main/java/com/blocksolid/retrofittutorial/MainActivity.java
+++ app/src/main/java/com/blocksolid/retrofittutorial/MainActivity.java
@@ -8,25 +8,73 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.blocksolid.retrofittutorial.api.GitApi;
+import com.blocksolid.retrofittutorial.model.GitModel;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button click;
+    TextView tv;
+    EditText edit_user;
+    ProgressBar pbar;
+    String API = "https://api.github.com";                         //BASE URL
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        click = (Button) findViewById(R.id.button);
+        tv = (TextView) findViewById(R.id.tv);
+        edit_user = (EditText) findViewById(R.id.edit);
+        pbar = (ProgressBar) findViewById(R.id.pb);
+        pbar.setVisibility(View.INVISIBLE);
+
+        click.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                String user = edit_user.getText().toString();
+                pbar.setVisibility(View.VISIBLE);
+
+                //Retrofit section start from here...
+                RestAdapter restAdapter = new RestAdapter.Builder()
+                        .setEndpoint(API).build();                                        //create an adapter for retrofit with base url
+
+                GitApi git = restAdapter.create(GitApi.class);                            //creating a service for adapter with our GET class
+
+                //Now ,we need to call for response
+                //Retrofit using gson for JSON-POJO conversion
+
+                git.getFeed(user, new Callback<GitModel>() {
+                    @Override
+                    public void success(GitModel GitModel, Response response) {
+                        //we get json object from github server to our POJO or model class
+
+                        tv.setText("Github Name :" + GitModel.getName() + "\nWebsite :" + GitModel.getBlog() + "\nCompany Name :" + GitModel.getCompany());
+
+                        pbar.setVisibility(View.INVISIBLE);                               //disable progressbar
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        tv.setText(error.getMessage());
+                        pbar.setVisibility(View.INVISIBLE);                               //disable progressbar
+                    }
+                });
             }
         });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
