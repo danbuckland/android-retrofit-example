b6fa6995d46d67cb02af26d0784d39592a690eb8
Dan Buckland <danbucklan@gmail.com>
2016-02-01 22:22:09 +0000

    Add okhttp3 logging

--- app/src/main/java/com/blocksolid/retrofittutorial/api/ServiceGenerator.java
+++ app/src/main/java/com/blocksolid/retrofittutorial/api/ServiceGenerator.java
@@ -1,6 +1,8 @@
 package com.blocksolid.retrofittutorial.api;
 
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.GsonConverterFactory;
 import retrofit2.Retrofit;
 
@@ -8,7 +10,19 @@ public class ServiceGenerator {
 
     public static final String API_BASE_URL = "https://api.github.com/";
 
-    private static OkHttpClient httpClient = new OkHttpClient();
+    private static HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+    private static Interceptor logging = interceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+
+//    Retrofit retrofit = new Retrofit.Builder()
+//            .baseUrl(API_BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .client(httpClient)
+//            .build();
+
+    private static OkHttpClient httpClient = new OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build();
+
     private static Retrofit.Builder builder =
             new Retrofit.Builder()
                     .baseUrl(API_BASE_URL)
